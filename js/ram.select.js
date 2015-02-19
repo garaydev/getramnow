9731ad6ce489586d250d5c557b961e26a2aa0e20
Angel G <garaydev@gmail.com>
2015-02-18 22:55:18 -0500

    Third commit. Verify Browser update.

--- js/ram.select.js
+++ js/ram.select.js
@@ -5,8 +5,13 @@ Writted and maintained by Annie!
 */
 $(document).ready(function(){
     
-$(".btn-group > .btn").click(function(){
+$(".btn-group > .btn.ramSel").click(function(){
     $(this).addClass("active").siblings().removeClass("active");
+});
+    
+$("button > .fs1.browSel").click(function(){
+    $(".btn>span.fs1.browSel").removeClass("activeFS1");
+    $(this).addClass("activeFS1");
 });
     
 });
