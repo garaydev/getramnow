dd135ef621782db0ebccd007eac138fd71afc4e5
Angel G <garaydev@gmail.com>
2015-02-21 00:24:03 -0500

    Added Setup Wizard. Fix Download button

--- js/main.js
+++ js/main.js
@@ -6,9 +6,7 @@ Writted and maintained by Annie!
 $(document).ready(function(){
     
     $("#downloadBar").hide();
-    
-     //spyscroll
-    $('body').scrollspy({ target: '.navbar-collapse' });
+        
     
     //owl
      $("#owl-example").owlCarousel({
@@ -18,64 +16,42 @@ singleItem:true
 });
     
     //navigation animation code
-        $('.navs a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-               if (target.length) {
-                 $('html,body').animate({
-                     scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
+//        $('.navs a[href*=#]:not([href=#])').click(function() {
+//    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+//        || location.hostname == this.hostname) {
+//
+//            var target = $(this.hash);
+//            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//               if (target.length) {
+//                 $('html,body').animate({
+//                     scrollTop: target.offset().top
+//                }, 1000);
+//                return false;
+//            }
+//        }
+//    });
     
-    ( function( $ ) {
-    // Init Skrollr
-    var s = skrollr.init({
-        render: function(data) {
-            //Debugging - Log the current scroll position.
-            //console.log(data.curTop);
-        }
-    });
-} )( jQuery );
-		
+    //spyscroll
+    $('body').scrollspy({ target: '.navbar-collapse' });
     
 var $body   = $(document.body);
 var navHeight = $('.navbar').outerHeight(true) + 10;
 
-$body.scrollspy({
+       $body.scrollspy({
 	target: '#menuTop',
 	offset: navHeight
 });
    
+
+
     //remove collapse for navbar-bar when clicked
     $(function() {
-        $('.nav a').on('click', function(){ 
+        $('#menuTop .nav a').on('click', function(){ 
             if($('.navbar-toggle').css('display') !='none'){
                 $(".navbar-toggle").trigger( "click" );
             }
         });
     });
-    
-
-    //click download RAM button
-    $('#h-fill-animation-start').click(function() {
-    $('.progress .progress-bar').progressbar({
-        display_text: 'fill',
-        transition_delay: 1000,
-        done: function(){ $('#done').append('<h1> Yeah! You just downloaded RAM</h1>');
-                          $('#downloadBarSub').removeClass('progress-bar-success');
-                          $('#downloadBarSub').addClass('progress-bar-info')
-                          
-                        }
-    });
-    $('#downloadBar').slideDown( "slow" );
-   });
-    
     
 });
 
--- js/ram.select.js
+++ js/ram.select.js
@@ -5,13 +5,70 @@ Writted and maintained by Annie!
 */
 $(document).ready(function(){
     
+
+    //click download RAM button
+    $('#h-fill-animation-start').click(function() {
+    $('.progress .progress-bar').progressbar({
+        display_text: 'fill',
+        transition_delay: 1000,
+        done: function(){ $('#done').append('<h1> Yeah! You just downloaded RAM</h1>');
+                          $('#downloadBarSub').removeClass('progress-bar-success');
+                          $('#downloadBarSub').addClass('progress-bar-info')
+                          
+                        }
+    });
+    $('#downloadBar').slideDown( "slow" );
+   });
+
+$(".btn-group > .btn.ramSel").click(function(){
+    $(this).addClass("active").siblings().removeClass("active");
+    $("#activate-step-2").removeClass("disabled");
+});
+
 $(".btn-group > .btn.ramSel").click(function(){
     $(this).addClass("active").siblings().removeClass("active");
 });
-    
+
 $("button > .fs1.browSel").click(function(){
     $(".btn>span.fs1.browSel").removeClass("activeFS1");
     $(this).addClass("activeFS1");
+    $("#activate-step-3").removeClass("disabled");
 });
+    
+    
+    /* Start of SetupWizard */
+    var navListItems = $('ul.setup-panel li a'),
+        allWells = $('.setup-content');
+
+    allWells.hide();
+
+    navListItems.click(function(e)
+    {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+            $item = $(this).closest('li');
+        
+        if (!$item.hasClass('disabled')) {
+            navListItems.closest('li').removeClass('active');
+            $item.addClass('active');
+            allWells.hide();
+            $target.show();
+        }
+    });
+    
+    $('ul.setup-panel li.active a').trigger('click');
+
+    $('#activate-step-2').on('click', function(e) {
+        $('ul.setup-panel li:eq(1)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-2"]').trigger('click');
+        $(this).remove();
+    })
+    
+    $('#activate-step-3').on('click', function(e) {
+        $('ul.setup-panel li:eq(2)').removeClass('disabled');
+        $('ul.setup-panel li a[href="#step-3"]').trigger('click');
+        $(this).remove();
+    })  
+    
     
 });
