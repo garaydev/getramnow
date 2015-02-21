75a50532537978d572abf20f9a997a776deefbd6
Angel G <garaydev@gmail.com>
2015-02-21 08:12:23 -0500

    Fixing RAMSelect Click

--- js/ram.select.js
+++ js/ram.select.js
@@ -29,8 +29,8 @@ $(".btn-group > .btn.ramSel").click(function(){
     $(this).addClass("active").siblings().removeClass("active");
 });
 
-$("button > .fs1.browSel").click(function(){
-    $(".btn>span.fs1.browSel").removeClass("activeFS1");
+$("button > span.fs1.browSel").click(function(){
+    $("button > span.fs1.browSel").removeClass("activeFS1");
     $(this).addClass("activeFS1");
     $("#activate-step-3").removeClass("disabled");
 });
